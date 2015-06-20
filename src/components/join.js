7a31fb72fb95b046e0803c3ad8947294e780a865
Daiwei <daiweilu123@gmail.com>
2015-06-20 03:30:18 -0400

    Working num boxes

--- src/components/join.js
+++ src/components/join.js
@@ -5,7 +5,7 @@ import joinStyle from './join.scss';
 export default React.createClass({
 
   getInitialState() {
-    return { vals: [] };
+    return { value: '' };
   },
 
   input() {
@@ -14,8 +14,10 @@ export default React.createClass({
 
   inputChanged() {
     let input = this.input();
-    let vals = input.value.split('');
-    this.setState({ vals });
+    if (input.value.length > 4) {
+      input.value = input.value.slice(0, 4);
+    }
+    this.setState({ value: input.value });
   },
 
   boxTapped() {
@@ -23,13 +25,25 @@ export default React.createClass({
   },
 
   render() {
+    let boxes = [0, 1, 2, 4].map( n => {
+      return (
+        <div
+          className={ joinStyle['join-page__box'] }
+          onClick={ this.boxTapped }
+          key={ n }>
+          { this.state.value[n] }
+        </div>
+      );
+    })
     return (
       <div className={ `${pageStyle['page__inner']} ${joinStyle['join-page']}` }>
-        <input type='text' pattern='[0-9]*' className={ joinStyle['join-input'] } ref='input' onChange={ this.inputChanged }/>
-        <div className={ joinStyle['join-page__box'] } onClick={ this.boxTapped }>{ this.state.vals[0] }</div>
-        <div className={ joinStyle['join-page__box'] } onClick={ this.boxTapped }>{ this.state.vals[1] }</div>
-        <div className={ joinStyle['join-page__box'] } onClick={ this.boxTapped }>{ this.state.vals[2] }</div>
-        <div className={ joinStyle['join-page__box'] } onClick={ this.boxTapped }>{ this.state.vals[3] }</div>
+        <input
+          type='text'
+          pattern='[0-9]*'
+          className={ joinStyle['join-page__input'] }
+          ref='input'
+          onChange={ this.inputChanged } />
+        { boxes }
       </div>
     );
   },
